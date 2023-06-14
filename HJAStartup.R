352511e654a767229ea9d5eb6f5f2ec887786bcf
John Mallard <15877282+jmmallard@users.noreply.github.com>
2023-06-14 11:28:29 -0400

    Added startup script to speed package attachment and data loading.
    Added data structuring and plotting for other two Moore 2004 figures

---
+++ HJAStartup.R
@@ -0,0 +1,14 @@
+#Startup items:
+#Load packages
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(zoo)
+library(climwin)
+library(plotly)
+library(imputeTS)
+library(ggpubr)
+library(ggthemes)
+
+#Load working data
+load("WorkingData.Rdata")
